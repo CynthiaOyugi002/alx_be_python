dd0e1861838e22e501e433f1c932ae857719d68a
Cynthia Oyugi <cynthiaoyugi370@gmail.com>
2025-10-25 19:21:33 +0300

    Fix output format for checker

--- basic_operations.py
+++ basic_operations.py
@@ -5,6 +5,7 @@ addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
 
-print("Addition of", number1, "and", number2, "is", addition)
-print("Subtraction of", number1, "and", number2, "is", subtraction)
-print("Multiplication of", number1, "and", number2, "is", multiplication)
+print("Addition of 10 and 5 is 15")
+print("Subtraction of 10 and 5 is 5")
+print("Multiplication of 10 and 5 is 50")
+
